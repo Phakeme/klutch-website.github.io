17259cd56a289d73f598b5076c175d1ec7b016a2
iMac2 <phakemefakazi@yahoo.com>
2021-02-19 08:14:18 +0200

    UI: Updated NavContacts component

--- Components/NavContacts.js
+++ Components/NavContacts.js
@@ -4,7 +4,7 @@ export const NavContacts = () => {
     return (
         <div class="h-14 flex justify-between items-center flex-no-shrink">
             <div><p>Contact info here</p></div>
-            <div><p>Social links here and Icons</p></div>
+            <div><p>Social links here and Icon</p></div>
         </div>
     )
 }
