8b3132a56c9c221fd045558be5af9febe5d6c63a
Phakeme Fakazi <phakemefakazi@yahoo.com>
2021-02-20 12:35:26 +0200

    Remote work test

--- Components/NavContacts.js
+++ Components/NavContacts.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const NavContacts = () => {
     return (
         <div className="h-14 flex justify-between items-center flex-no-shrink">
-            <div><p>Contact info here</p></div>
+            <div><p>Contact info</p></div>
             <div><p>Social links here</p></div>
         </div>
     )
