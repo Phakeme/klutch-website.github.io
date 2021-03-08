9605fc89404af5edae5bb87ff5ce9b1b6558ddab
iMac2 <phakemefakazi@yahoo.com>
2021-03-08 14:28:40 +0200

    Fixing error on production

--- pages/contacts.js
+++ pages/contacts.js
@@ -1,12 +1,12 @@
 import { FooterContainer } from "../containers/global/footer";
-import { ContactUsContainer } from "../containers/pages/contact/contact";
+import { FormContainer } from "../containers/global/form";
 import { HeaderContainer } from "../containers/pages/home/header";
 
 export default function FirstPost() {
   return (
     <>
       <HeaderContainer />
-      <ContactUsContainer />
+      <FormContainer />
       <FooterContainer />
     </>
   );
