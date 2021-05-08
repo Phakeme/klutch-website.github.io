f836548c49a0de654377aa37d15d92c241077b3c
Phakeme Fakazi <phakemefakazi@yahoo.com>
2021-05-08 18:13:40 +0200

    Added Sanity CMS

---
+++ client.js
@@ -0,0 +1,8 @@
+const sanityClient = require('@sanity/client')
+export default sanityClient({
+    projectId: process.env.PROJECT_ID,
+    dataset: process.env.DATASET,
+    apiVersion: process.env.API_VERSION, // use current UTC date - see "specifying API version"!
+    token: process.env.TOKEN, // or leave blank for unauthenticated usage
+    useCdn: process.env.USE_CDN, // `false` if you want to ensure fresh data
+})
--- containers/pages/procedures/ProceOverview.js
+++ containers/pages/procedures/ProceOverview.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ProceOverview } from "../../../components";
+import sanityClient from '../../../client'
 
 export function ProceOverviewContainer() {
+  const [procedure, setProdure] = useState([])
+
+  useEffect(() => {
+    sanityClient.fetch(`*[_type == "post"]{
+      title,
+      slug,
+    }`).then(data => setProdure(data)).catch(console.error)
+  }, [])
   return (
     <ProceOverview>
       <ProceOverview.Wrapper>
+        {procedure.map((item, index) => (
+          <div key={index}>
+            <p>{item.title}</p>
+          </div>
+        ))}
       </ProceOverview.Wrapper>
     </ProceOverview>
   );
