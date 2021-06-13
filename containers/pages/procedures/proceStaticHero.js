5bf374c9d9c2468ddfd15fd3cb157a3a49fb4124
Phakeme Fakazi <phakemefakazi@yahoo.com>
2021-06-13 23:24:21 +0200

    UI: Added a dynamic HeroSection

--- containers/pages/procedures/proceStaticHero.js
+++ containers/pages/procedures/proceStaticHero.js
@@ -1,16 +1,16 @@
-import React from "react";
-import { ProceHero } from "../../../components";
+// import React from "react";
+// import { ProceHero } from "../../../components";
 
-export function ProceStaticHeroContainer() {
-  return (
-    <ProceHero>
-      <ProceHero.Wrapper>
-        <ProceHero.TextBox>
-          <h1>Top Female Plastic Surgeon in Durban.</h1>
-          <p>
-            Committment to excellence and attention to details.</p>
-        </ProceHero.TextBox>
-      </ProceHero.Wrapper>
-    </ProceHero>
-  );
-}
+// export function ProceStaticHeroContainer() {
+//   return (
+//     <ProceHero>
+//       <ProceHero.Wrapper>
+//         <ProceHero.TextBox>
+//           <h1>Top Female Plastic Surgeon in Durban.</h1>
+//           <p>
+//             Committment to excellence and attention to details.</p>
+//         </ProceHero.TextBox>
+//       </ProceHero.Wrapper>
+//     </ProceHero>
+//   );
+// }
